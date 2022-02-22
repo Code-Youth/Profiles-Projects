a7e18de50db663af23bf0128478d60da5fa87a93
Gursheen Kaur <gursheen1436@gmail.com>
2022-02-22 14:28:09 -0500

    add profiles using .js with new layouts

--- db.js
+++ db.js
@@ -1,80 +1,80 @@
 //  user database
 const users = [
     {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'Gursheen',
+        img: 'https://media-exp1.licdn.com/dms/image/C4D03AQE7UZlwJHEkIg/profile-displayphoto-shrink_400_400/0/1639791051679?e=1645660800&v=beta&t=S8HEHH8hXdfw6SkGccn7EnCaE03pcsDNqMrj1RXn918',
+        name: 'Gursheen Chawla',
         bio: 'I am a Web Developer, Cyber-Security analyst, Mother and a natural problem solver. My multi-tasking skills make me more rigid and positive about my career goals.',
         location: 'Ottawa, ON',
-        github: 'https://www.gursheen.dev/',
+        github: 'https://github.com/gursheens',
         linkedin: 'https://www.linkedin.com/in/gursheenk/',
-        portfolio: 'https://gursheens.github.io/'
+        portfolio: 'https://www.gursheen.dev/'
     },
     {
         img: 'images/Shaya.jpg',
-        name: 'Shaya',
+        name: 'Shaya Mukhtar',
+        bio: 'Having spent a large chunk of my life in art school, I have finally found my passion at the crossroads of design and technology. I aim to secure an IT position in a recognized organization to further my education, experience, and skills.',
+        location: 'Ottawa, ON',
+        github: 'https://github.com/ShayaMukhtar',
+        linkedin: 'https://www.linkedin.com/in/shayamukhtar/',
+        portfolio: 'https://shayas-portfolio.netlify.app/'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E03AQEAlE_Yvl_d6g/profile-displayphoto-shrink_400_400/0/1640026708989?e=1645660800&v=beta&t=dEbLKD3t6HXfXW08kVk5X2-v00xjwRX3q7F0CDvsY8M',
+        name: 'Ali Alhattab',
+        bio: 'I am a high-energy individual with a detail-oriented and people-friendly nature. True team player always willing to go the extra mile. Pursuing a position in the IT sector where exceptional skills are valued.',
+        location: 'Ottawa, ON',
+        github: 'https://github.com/AliAlhattab',
+        linkedin: 'https://www.linkedin.com/in/alialhattab/',
+        portfolio: 'https://alialhattab-portfolio.netlify.app/'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C5603AQHZHoP7ELYOiw/profile-displayphoto-shrink_400_400/0/1538099459124?e=1645660800&v=beta&t=Mkbre6c1DuFvs5RQPoFQ-qobnoZmf1HVkxlmSbOFrM0',
+        name: 'Jun Lu',
+        bio: 'A self-motivated and detail-oriented professional fast-paced dynamic financial institutions.',
+        location: 'Toronto, ON',
+        github: 'https://github.com/AnfieldCat',
+        linkedin: 'https://www.linkedin.com/in/junrlu/',
+        portfolio: 'link'
+    },
+    {
+        img: 'images/stephen.jfif',
+        name: 'Stephen Ward',
+        bio: 'some textThe code Stephen wrote allows people to make a game of breakout with arbitrary text instead of normal blocks. The example shown is Stephens portfolio gamified using this code.',
+        location: 'Brockville, ON',
+        github: 'https://github.com/robotic-gamer',
+        linkedin: 'https://www.linkedin.com/in/stephen-ward-robotic/',
+        portfolio: 'link'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D03AQFVLcwyQg7PJQ/profile-displayphoto-shrink_200_200/0/1640371511138?e=1650499200&v=beta&t=vbxbX-t9iMncz8bbTMIVvyr-8pOTAicHodSEQUI_UVo',
+        name: 'Prisca Azulphar',
+        bio: 'My name is Prisca, a young entrepreneur that loves to work hard and that is passionate and perseververante. I recently finished a 3 month bootcamp at code youth that was teaching different languages in web development. I am in the process of building my own company.',
+        location: 'Ottawa, ON',
+        github: 'https://github.com/priscaaz',
+        linkedin: 'https://www.linkedin.com/in/priscaaulphar/',
+        portfolio: 'https://prisca.dev/'
+    },
+    {
+        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
+        name: 'Majid Mukhtar',
+        bio: 'I am junior web developer that enjoy creating solutions to solve problems that I care about. I am always learning and trying to expand my knowledge as a programmer.',
+        location: 'Ottawa, ON',
+        github: 'https://github.com/MajidMukhtar?tab=repositories',
+        linkedin: 'https://linkedin.com/in/majid-mukhtar',
+        portfolio: 'https://majidmukhtar.dev/'
+    },
+    {
+        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
+        name: 'Carter Gillis',
         bio: 'some text',
-        location: 'area',
+        location: 'Oshawa, ON',
         github: 'link',
-        linkedin: 'link',
+        linkedin: 'https://www.linkedin.com/in/carter-j-gillis/',
         portfolio: 'link'
     },
     {
         img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'Ali',
-        bio: 'some text',
-        location: 'area',
-        github: 'link',
-        linkedin: 'link',
-        portfolio: 'link'
-    },
-    {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'Jun',
-        bio: 'some text',
-        location: 'area',
-        github: 'link',
-        linkedin: 'link',
-        portfolio: 'link'
-    },
-    {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'Stephen',
-        bio: 'some text',
-        location: 'area',
-        github: 'link',
-        linkedin: 'link',
-        portfolio: 'link'
-    },
-    {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'jannat',
-        bio: 'some text',
-        location: 'area',
-        github: 'link',
-        linkedin: 'link',
-        portfolio: 'link'
-    },
-    {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'jannat',
-        bio: 'some text',
-        location: 'area',
-        github: 'link',
-        linkedin: 'link',
-        portfolio: 'link'
-    },
-    {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'jannat',
-        bio: 'some text',
-        location: 'area',
-        github: 'link',
-        linkedin: 'link',
-        portfolio: 'link'
-    },
-    {
-        img: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
-        name: 'jannat',
+        name: 'Future Students',
         bio: 'some text',
         location: 'area',
         github: 'link',
